c47f674fd7e962eb82a4956ea60e97559df25de0
Kevin Rohling <kevin@kevinrohling.com>
2016-03-28 18:26:01 -0700

    don't compile assets in prod

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -13,10 +13,6 @@
  */
 module.exports = function(grunt) {
   grunt.registerTask('prod', [
-    'compileAssets',
-    'concat',
-    'uglify',
-    'cssmin',
     'sails-linker:prodJs',
     'sails-linker:prodStyles',
     'sails-linker:devTpl',
