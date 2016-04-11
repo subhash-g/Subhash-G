7c53b23e83f0d1a40889c82d768e75bbba7c6a4b
Kevin Rohling <kevin@kevinrohling.com>
2016-04-11 12:20:07 -0700

    add diginomica

--- config/customers.js
+++ config/customers.js
@@ -9,6 +9,99 @@ module.exports = {
 		userProperties: [],
 		userLists: []
 	},
+	'2987': {
+		name: 'Diginomica',
+		logo: '/images/logos/dgn.jpg',
+		bmeApiKey: '5db778a4bd7c36cc93812b2f1242ac17',
+		userProperties: [
+			{
+				name: 'First Name',
+				property: 'first_name'
+			},
+			{
+				name: 'Last Name',
+				property: 'last_name'
+			},
+		],
+		userLists: [
+			{
+				name: 'All you can eat (everything)',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Analytics planning and data analysis',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Cloud ERP financials and supply chain',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Cloud platforms - infrastructure and architecture',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Collaboration sharing and digital productivity',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'CRM and customer experience',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'DevOps NoSQL and the open source stack',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Digital and content marketing',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Digital enterprise in the real world',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Digital government and public services',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Digital transformation - frictionless enterprise',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Financial services and fintech',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Governing identity privacy and security',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'HCM and the digital future of work',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Healthcare transformation',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'IoT robotics and AI',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'Retail e-commerce and the omni-channel',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'The new professional and IT as a service',
+				property: 'properties.subscribe_all'
+			},
+			{
+				name: 'UX and application design',
+				property: 'properties.subscribe_all'
+			},
+		],
+	},
 	'1496': {
 		name: 'Joy of Kosher',
 		logo: '/images/logos/joy-of-kosher.png',
