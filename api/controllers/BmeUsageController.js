129180266858b1570b4e6add347da0951a9a6ff2
Kevin Rohling <kevin@kevinrohling.com>
2016-04-30 15:25:36 -0700

    add auth, top sender and subscriber reports

--- api/controllers/BmeUsageController.js
+++ api/controllers/BmeUsageController.js
@@ -2,24 +2,50 @@ var url = require('url') ;
 var bme = require('../../lib/bme');
 
 module.exports = {
-
+  login: function(req, res) {
+    return res.view("bmeusage/login");
+  },
+  authenticate: function(req, res) {
+    if(req.body.user.email == "reports@boomtrain.com" && req.body.user.password == "b00mtra1n") {
+      req.session.authenticated = true;
+      res.redirect("/usage/bme");
+    }
+    else {
+      return res.view("bmeusage/login");
+    }
+  },
   index: function(req, res) {
-    //954b37f3-1c51-4ac7-9868-fce9c75d29ab
-    var queryObject = url.parse(req.url,true).query;
-    if(queryObject.access_token == "954b37f3-1c51-4ac7-9868-fce9c75d29ab") {
+    if(req.session.authenticated) {
       return res.view('bmeusage/index');
     }
     else {
-      return res.view('404');
+      return res.redirect("/usage/bme/login");
+    }
+  },
+  topSenders: function(req, res) {
+    if(req.session.authenticated) {
+      return res.view('bmeusage/top_senders');
+    }
+    else {
+      return res.redirect("/usage/bme/login");
+    }
+  },
+  topSubscribers: function(req, res) {
+    if(req.session.authenticated) {
+      return res.view('bmeusage/top_subscribers');
+    }
+    else {
+      return res.redirect("/usage/bme/login");
     }
   },
   billingData: function (req, res) {
     var queryObject = url.parse(req.url,true).query;
 	  var cycles = queryObject.cycles;
     
-    if(queryObject.access_token == "954b37f3-1c51-4ac7-9868-fce9c75d29ab") {
+    if(req.session.authenticated) {
       bme.getBillingCycles(cycles, function(data, error) {
         if(error == null) {
+          //console.log(JSON.stringify(data));
           res.json(200, data);
         }
         else {
--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,10 @@ module.exports.routes = {
 	'GET /preferences/:customerId/users': 'PreferencesController.index',
 	'GET /preferences/:customerId/users/:userId': 'PreferencesController.index',
 	'POST /preferences/:customerId/users/:userId': 'PreferencesController.update',
-	'GET /usage/bme': 'BmeUsageController.index',
+	'GET /usage/bme/login': 'BmeUsageController.login',
+	'POST /usage/bme/login': 'BmeUsageController.authenticate',
 	'GET /usage/bme/billing': 'BmeUsageController.billingData',
+	'GET /usage/bme': 'BmeUsageController.index',
+	'GET /usage/bme/top_senders': 'BmeUsageController.topSenders',
+	'GET /usage/bme/top_subscribers': 'BmeUsageController.topSubscribers',
 };
