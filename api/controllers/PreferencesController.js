e0e14afe4843825029a33a266733376bc1aabe5f
Derek Van de Streek <Derek@Dereks-MacBook-Air.local>
2016-07-05 16:24:59 -0700

    Added Comments

--- api/controllers/PreferencesController.js
+++ api/controllers/PreferencesController.js
@@ -1,3 +1,4 @@
+// What is meaning of the text included within the single quotes?
 var url = require('url') ;
 var customers = require('../../config/customers');
 var bme = require('../../lib/bme');
@@ -7,6 +8,12 @@ module.exports = {
 
   index: function (req, res) {
 	var queryObject = url.parse(req.url,true).query
+
+	console.log(req);
+
+	//REMOVE ME
+	console.log(queryObject);
+
 	var userId = req.params.userId || queryObject.userId || queryObject.email;
 	var originalUserId = userId;
 	
@@ -121,29 +128,70 @@ module.exports = {
 	
   },
   update: function (req, res) {
+
+  	//Creates an object queryObject parsed from req.url. The query property will be set to an object returned by the 
+  	//querystring module's parse() method. 
 	var queryObject = url.parse(req.url,true).query
+
+	// Creates an object userId set to the userId of req (the object contraining information abotu the HTTP request that
+	// raised the event) of the userId of queryObject (the object ???)
 	var userId = req.params.userId || queryObject.userId;
 	
+	// Creates an object customerId set to req's (the object containing information about the HTTP reuqest that 
+	// raised the event) of params field's customerIF field. 
 	var customerId = req.params.customerId;
+
+	// Creates an object customer set to the customerId element within customers.js
 	var customer = customers[customerId];
 
+	// Creates an object preferences. Calls the buildPreferences helper method with arguments req.body (data), 
+	// customer.userProperties (userProperties), and customer.userLists (userLists). req.body is ???, 
+	// customer.userProperties is ???, and customer.userLists is ???. 
+	//
+	// Q - What is this in this case and why is buildPreferences called on this while other helper methods are called on 
+	// module.exports?
 	var preferences = this.buildPreferences(req.body, customer.userProperties, customer.userLists);
 	
+	// Calls updateUser upon the bme object. userId is ???, customer. bmeApiKey is ???, preferences is the property to be updated
+	// and the callback function is:
 	bme.updateUser(userId, customer.bmeApiKey, preferences, function(data, error) {
+
+		console.log(data);
+
+		// If error is null
 		if(error == null) {
+
+			// Iterate over the length of data's contacts array. 
 			for(var x = 0; x < data.contacts.length; x++){
+
+				// If an element within the contacts array has a contact_type of 'email'...
 				if(data.contacts[x].contact_type === 'email'){
+
+					// Create a variable userSubscriber set to the current element within the contacts array and break.
 					var userSubscriber = data.contacts[x];
 					break;
 				}
 			}
+
+			// Create an object subscriberProps with the key object subscriber_contact.
 			var subscriberProps = {
+
+				// Creates an object subscriber_contact with keys contact_type, contact_value, and subscription_status
 				'subscriber_contact':{
+
+					// Set contact_type to 'email'
 					'contact_type':'email',
+
+					// If preferences's contact_email field is not undefined, set contact_value to preferences's
+					// contact email field, otherwise set contact_value to userSubscriber's contact_value field. 
 					'contact_value':preferences.contact_email != undefined ? preferences.contact_email : userSubscriber.contact_value,
+					
+					// Set subscription_status to 'active'
 					'subscription_status': 'active'
 				}
 			}
+
+
 			bme.updateSubscriber(userSubscriber.id, customer.bmeApiKey, subscriberProps, function(data, error){
 				if(error == null) {
 					res.json({});
@@ -156,6 +204,8 @@ module.exports = {
 			});
 			
 		}
+
+
 		else {
 			res.json(400, {
 				error: error.message
@@ -163,10 +213,27 @@ module.exports = {
 		}
 	});
   },
+
+  /* Called in the update function when instantiating object preferences. 
+	- data : array 
+	- userProperties : ?
+	- userLists : ?
+  */
   buildPreferences: function(data, userProperties, userLists) {
+
+  	// Creates an object result. 
 	var result = {};
+
+	// Uses a callback function for userProperties. Executes the inner code once per array element. (What array are 
+	// we in reference to?)
 	userProperties.forEach(function(item) {
+
+		// Creates a variable value set to the property field of item within the data array. Where is the data array
+		// and how do we refer to it?
 		var value = data[item.property];
+
+		// Calls setUserPropertyVale on module.exports with arguments result, item.property and value. result is ???, 
+		// item.property is ???, and value is ???. 
 		module.exports.setUserPropertyValue(result, item.property, value);
 	});
 	
@@ -177,21 +244,50 @@ module.exports = {
 	
 	return result;
   },
+
+  /* 
+  	Called in the buildPreferences function.
+
+  	- data : array 
+  	- property : string that is delimited by periods
+  	- value : ?
+  */
   setUserPropertyValue: function(data, property, value) {
+
+  	// Creates an array path set to the property string delimited by periods. 
 	var path = property.split(".");
+
+	// Calls setUserProperty value. data is ???, path is ???, value is ???, and 1
 	this.setUserPropertyValueHelper(data, path, value, 1);
   },
+
+  /*
+	Called by the setUserPropertyValue function.
+	
+	- data : array 
+	- path : array 
+	- value : variable in which the data array's element at index key may be set to. 
+	- depth : integer 
+  */
   setUserPropertyValueHelper: function(data, path, value, depth) {
+
+  	// Creates a variable key that is the depth - 1st element within the path array. 
 	var key = path[depth-1];
 	
+	// If the capacity of the path array is equal to the depth value, set the data array's element at index key to 
+	// value. 
 	if(path.length == depth) {
 		data[key] = value;
 	}
+
+	// Otherwise if the data array's element at index key is null, set the value of the data array at index key to {}
+	// What is {}?
 	else {
 		if(data[key] == null) {
 			data[key] = {};
 		}
 		
+		// Recursively call the setUserPropertyValue Helper method witht the depth incremented by one. 
 		this.setUserPropertyValueHelper(data[key], path, value, depth+1);
 	}
   },
@@ -213,5 +309,10 @@ module.exports = {
 		return this.getUserPropertyValueHelper(value, path, depth+1);
 	}
   },
+
+  // Derek's tests, delete this later!
+  derekTest: function() {
+  	console.log(url.parse(req.url,true).query);
+  }
 
 };
